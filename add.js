1a69e097ec7ac8677a709b15429d171e90790a82
Francis Ngo <francis.t.ngo@gmail.com>
2018-03-07 09:00:20 -0800

    Complete four toy problems

---
+++ add.js
@@ -0,0 +1,16 @@
+/*
+
+Write a function that returns the sum of two numbers.
+
+Example
+
+For param1 = 1 and param2 = 2, the output should be
+add(param1, param2) = 3.
+
+Link to problem: https://codefights.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa
+
+*/
+
+function add(param1, param2) {
+  return param1 + param2;
+}
---
+++ adjacentElementsProduct.js
@@ -0,0 +1,25 @@
+/*
+
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+
+7 and 3 produce the largest product.
+
+Link to problem: https://codefights.com/arcade/intro/level-2/xzKiBHjhoinnpdh6m
+
+*/
+
+function adjacentElementsProduct(inputArray) {
+  let largest = -1000;
+  for (let i = 0; i < inputArray.length; i++) {
+    let product = inputArray[i] * inputArray[i + 1];
+    if (product >= largest) {
+      largest = product;
+    }
+  }
+  return largest;
+}
